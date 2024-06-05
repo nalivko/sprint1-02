c7f4b2a09d3995cf6fcacfe75c0913540a03e1ae
Dmitriy <nalivko.dmitr@gmail.com>
2024-06-05 17:32:14 +0300

    add delete method for tests

--- src/db/db.ts
+++ src/db/db.ts
@@ -10,3 +10,21 @@ export const db: DBType = {
     blogs: [],
     posts: []
 }
+
+export type ReadonlyDBType = { // тип для dataset
+    blogs: Readonly<BlogDbType[]>
+    posts: Readonly<PostDbType[]>
+    // some: any[]
+}
+
+export const setDB = (dataset?: Partial<ReadonlyDBType>) => {
+    if(!dataset) {
+        db.blogs = []
+        db.posts = []
+
+        return
+    }
+
+    db.blogs = dataset.blogs?.map(blog => ({...blog})) || db.blogs
+    db.posts = dataset.posts?.map(post => ({...post})) || db.posts
+}
